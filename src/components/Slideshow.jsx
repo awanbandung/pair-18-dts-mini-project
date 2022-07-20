5f5e856edaa39e8a9757ac7b89930260d2b2086e
Wawan Rudiana N <awanbandung@gmail.com>
2022-07-21 06:50:50 +0700

    add

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -4,7 +4,7 @@ import Carousel from "react-bootstrap/Carousel";
 export default function Slideshow() {
   return (
     <div>
-      <Carousel fade style={{ }}>
+      <Carousel fade style={{}}>
         <Carousel.Item  >
           <img
             style={{
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Footer from '../components/Footer';
 import Header from '../components/Header'
-import Slideshow from '../components/Slideshow'
 
 
 import ListMoviesReal from "../container/ListMoviesReal";
@@ -9,7 +8,7 @@ export default function HomePage() {
   return (
     <div>
         <Header/>
-        <Slideshow/>
+
 
         <div style={{  alignContent: 'center', alignItems: 'center'}}>
         <ListMoviesReal/>
